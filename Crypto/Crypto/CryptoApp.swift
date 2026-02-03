ba4259bb46bdfeada4d1af5b1e0ff5417c98c913
tiago <tiagoribeiro15111@gmail.com>
2026-02-03 15:28:35 +0000

    setUp project

---
+++ Crypto/Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by tiago on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
