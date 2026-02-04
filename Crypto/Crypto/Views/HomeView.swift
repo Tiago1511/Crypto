cca57c0e31d047b6062cd83532c5815d02a755cb
tiago <tiagoribeiro15111@gmail.com>
2026-02-04 15:12:00 +0000

    Create Tab Bar struct

---
+++ Crypto/Crypto/Views/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  HomeView.swift
+//  Crypto
+//
+//  Created by tiago on 04/02/2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Color.background
+                .ignoresSafeArea(.all)
+        }.background(){
+            Color.background
+                .ignoresSafeArea(.all)
+        }
+            
+        
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Crypto/Crypto/Views/WalletListView.swift
@@ -0,0 +1,21 @@
+//
+//  WalletView.swift
+//  Crypto
+//
+//  Created by tiago on 04/02/2026.
+//
+
+import SwiftUI
+
+struct WalletListView: View {
+    
+    var body: some View {
+        Text("Wallet")
+
+        
+    }
+}
+
+#Preview {
+    WalletListView()
+}
